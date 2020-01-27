a42a43acabbcb58395b36dba85c0fcf56ba28aaf
ftmzyldrm <ozyildirim.fatma@gmail.com>
2020-01-27 09:56:27 +0300

    first commit

---
+++ src/main/java/com/assignment/subscriber/subscriberassignment/SubscriberAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.subscriber.subscriberassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriberAssignmentApplication.class, args);
+    }
+
+}
